f62c5bd19ac4599162131b06cdba511d28c5a636
abhisheksahu1011 <sahuabhishek1011@gmail.com>
2022-09-21 22:15:38 +0530

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Footer.css'
+function Footer() {
+  return (
+    <div>
+         <footer>
+                <div class = "social-links">
+                <a href = "https://github.com/abhisheksahu1011?tab=repositories"><i class="fa-brands fa-github"></i></a>
+                <a href = "https://www.linkedin.com/in/abhisheksahu1011/"><i class="fa-brands fa-linkedin"></i></a>
+                <a href = "#Contact"><i class="fa-solid fa-envelope"></i></a>
+                <a href = "#Contact"> <i class="fa-solid fa-phone"></i></a>
+                </div>
+                <span>Made by Abhishek Sahu</span>
+            </footer>
+    </div>
+  )
+}
+
+export default Footer
